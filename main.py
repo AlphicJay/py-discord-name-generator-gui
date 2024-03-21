8943fe462a73f6e28a1f7bf2f5c4fb08d87fc645
Joseph Borne <josephpbourne@outlook.com>
2024-03-21 15:06:03 -0700

    import neccessary modules for app

---
+++ main.py
@@ -0,0 +1,5 @@
+# imported required modules
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+
+from random import choice
